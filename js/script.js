5e1fe8c2e5a9ac5db5890480485d4a2975f1c9c3
ssuubii <tmfor1230@naver.com>
2024-09-10 17:52:39 +0900

    뭔가.. 수정함 ..

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(function () {
   // modal
-  $("slides-video").click(function () {
+  $("slides-video").click(function (e) {
+    e.preventDefault();
     $("#modal").addClass("active");
   });
   $(".modal-btn").click(function () {
