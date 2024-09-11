2d6989054c1ddabb0b3f39609d80b7ea3c56343e
ssuubii <tmfor1230@naver.com>
2024-09-11 17:57:36 +0900

    오늘도 모달 만지작..

--- js/script.js
+++ js/script.js
@@ -1,10 +1,33 @@
-$(function () {
-  // modal
-  $("slides-video").click(function (e) {
-    e.preventDefault();
-    $("#modal").addClass("active");
+// $(function () {
+//   // modal
+//   $("video-list").click(function (e) {
+//     e.preventDefault();
+//     $("#modal").addClass("active");
+//   });
+//   $(".modal-btn").click(function () {
+//     $("#modal").removeClass("active");
+//   });
+// });
+
+// 모달 열기
+const modalTriggers = document.querySelectorAll(".modal-trigger");
+const modal = document.getElementById("modal");
+const closeModalBtn = document.getElementById("close-modal");
+
+modalTriggers.forEach((trigger) => {
+  trigger.addEventListener("click", function () {
+    modal.style.display = "block";
   });
-  $(".modal-btn").click(function () {
-    $("#modal").removeClass("active");
-  });
+});
+
+// 모달 닫기
+closeModalBtn.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+// 모달 외부 클릭 시 닫기
+window.addEventListener("click", function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 });
